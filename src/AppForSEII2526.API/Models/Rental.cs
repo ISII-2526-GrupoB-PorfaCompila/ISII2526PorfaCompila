f52e2ea172c5bf4d9205dad2f55c26efd3e3879a
ÁNGEL BARCELÓ PARADA <Angel.Barcelo@alu.uclm.es>
2025-10-04 11:33:02 +0200

    Creado rental y añadidos los atributos y los paymentmethod #21

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,15 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public double TotalPrice { get; set; }
+        public string DeliveryCarDealer { get; set; }
+        public string Name { get; set; } //creo que esto y surname hay que sustituirlo por ApplicationUser
+        public string Surname { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime RentingDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
