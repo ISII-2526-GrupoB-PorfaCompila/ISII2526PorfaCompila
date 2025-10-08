a2acf8379386172df139459e3ac210a1608b8def
Elias100cas <149321371+Elias100cas@users.noreply.github.com>
2025-10-08 11:32:21 +0200

    #15 Creado PurchaseItem y realizadas las conexion con Purchase modificando este último (#34)
    Aprobado

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -4,29 +4,50 @@
     {
         public Purchase()
         {
+            PurchaseItems = new List<PurchaseItem>();
         }
-        public Purchase(int purchaseId, string name, string surname, string deliveryCarDealer, DateTime purchasingDate, decimal purchasingPrice, PaymentMethod paymentMethod)
+
+        //public Purchase(int purchaseId, string name, string surname, string deliveryCarDealer, DateTime purchasingDate, ApplicationUser applicationUser, IList<PurchaseItem> purchaseItems, PaymentMethod paymentMethod)
+        //    : this(0,name, surname, deliveryCarDealer, purchasingDate, 0, applicationUser, purchaseItems, paymentMethod)
+        //{            
+        //}
+
+        public Purchase(int purchaseId, string name, string surname, string deliveryCarDealer, DateTime purchasingDate, decimal purchasingPrice, ApplicationUser applicationUser,IList<PurchaseItem> purchaseItems, PaymentMethod paymentMethod)
         {
+            PurchasingPrice = decimal.Round(purchaseItems.Sum(pi => pi.Price * pi.Quantity),2);
+
             Id = purchaseId;
             Name = name;
             Surname = surname;
             DeliveryCarDealer = deliveryCarDealer;
             PurchasingDate = purchasingDate;
-            PurchasingPrice = purchasingPrice;
+            ApplicationUser = applicationUser;
+            PurchaseItems = purchaseItems;
             PaymentMethod = paymentMethod;
         }
 
         [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Surname { get; set; }
 
         [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
         [Display(Name = "Delivery Car Dealer")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please, set your Delivery car dealer.")]
         public string DeliveryCarDealer { get; set; }
+
         public DateTime PurchasingDate { get; set; }
+
+        [Precision(10, 2)]
         public decimal PurchasingPrice { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public IList<PurchaseItem> PurchaseItems { get; set; }
+
+        [Display(Name = "Payment Method")]
         public PaymentMethod PaymentMethod { get; set; }
     }
 }
---
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(CarId), nameof(PurchaseId))]
+    public class PurchaseItem
+    {
+        public PurchaseItem()
+        {
+        }
+        public PurchaseItem(Purchase purchase, Car car, int quantity)
+        {
+            Purchase = purchase;
+            PurchaseId = purchase.Id;
+            Car = car;
+            CarId = car.Id;
+            Quantity = quantity;
+        }
+        public Purchase Purchase { get; set; }
+
+        public int PurchaseId { get; set; }
+
+        public Car Car { get; set; }
+
+        public int CarId { get; set; }
+
+        [Precision(10, 2)]
+        public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "You must provide a quantity higher than 1")]
+        public int Quantity { get; set; }
+    }
+}
