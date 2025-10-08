e1c1cf8b0f19f4f39e1e312e238e3fc116cc0e62
CARLOS JAVIER CAMACHO MARCHANTE <CarlosJavier.Camacho@alu.uclm.es>
2025-10-08 14:38:11 +0200

    Modificar clase BookingItem #54

--- src/AppForSEII2526.API/Models/BookingItem.cs
+++ src/AppForSEII2526.API/Models/BookingItem.cs
@@ -1,24 +1,25 @@
 ﻿namespace AppForSEII2526.API.Models
 {
-public class BookingItem
-{
-    public int BookingId { get; set; }
-    public int MantId { get; set; }
-    [StringLength(50, ErrorMessage = "El comentario es obligatorio. Debe tener como maximo 50 caracteres.", MinimumLength = 1)]
-    public string Comment { get; set; }
-    public Booking Booking { get; set; }
-    public Maintenance Maintenance { get; set; }
-    public BookingItem()
+    [PrimaryKey(nameof(BookingId), nameof(MantId))]
+    public class BookingItem
     {
-        
-    }
-    public BookingItem(int bookingId, int mantId, string comment, Booking booking, Maintenance maintenance)
-    {
-        BookingId = bookingId;
-        MantId = mantId;
-        Comment = comment;
-        Booking = booking;
-        Maintenance = maintenance;
-    }
+        public int BookingId { get; set; }
+        public int MantId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El comentario es obligatorio. Introduzca un comentario.")]
+        public string Comment { get; set; }
+        public Booking Booking { get; set; }
+        public Maintenance Maintenance { get; set; }
+        public BookingItem()
+        {
+
+        }
+        public BookingItem(int bookingId, int mantId, string comment, Booking booking, Maintenance maintenance)
+        {
+            BookingId = bookingId;
+            MantId = mantId;
+            Comment = comment;
+            Booking = booking;
+            Maintenance = maintenance;
+        }
     }
 }
