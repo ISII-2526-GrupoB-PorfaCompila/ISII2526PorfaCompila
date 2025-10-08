4b412f0fda37a3b426275f395c65bc821693aea1
ÁNGEL BARCELÓ PARADA <Angel.Barcelo@alu.uclm.es>
2025-10-08 18:51:42 +0200

    Creada clase RentalItem y añadidos todos los atriburos #20

---
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.Models
+{
+
+    [PrimaryKey(nameof(CarId), nameof(RentalId))]
+    public class RentalItem
+    {
+        public RentalItem(Car car, Rental rental, int rentalId, int carId, int quantity)
+        {
+            Car = car;
+            Rental = rental;
+            RentalId = rentalId;
+            CarId = carId;
+            Quantity = quantity;
+        }
+
+        // relaciones
+        public Car Car { get; set; }
+        public Rental Rental { get; set; }
+
+        [Required]
+        public int RentalId { get; set; }
+        
+        [Required]
+        public int CarId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "You must provide a quantity higher than 1")]
+        public int Quantity { get; set; }
+
+    }
+}
