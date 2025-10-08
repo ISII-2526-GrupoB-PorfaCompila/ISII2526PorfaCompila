d5e724a935cf9592cf2f57a318145f634cc15ffc
PacoSanchez26 <160671423+PacoSanchez26@users.noreply.github.com>
2025-10-08 13:13:46 +0200

    Creada la clase Review. (#33)
    Aprobada

---
+++ src/AppForSEII2526.API/Models/Review.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Review
+    {
+        public Review()
+        {
+        }
+
+        public Review(int id, string userName, string country, DateTime created, DriverTypes driverType)
+        {
+            Id = id;
+            UserName = userName;
+            Country = country;
+            Created = created;
+            DriverType = driverType;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        [Display(Name = "Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Created {  get; set; }
+
+        [Required]
+        public DriverTypes DriverType { get; set; }
+    }
+
+    public enum DriverTypes
+    {
+        Novato,
+        Experto
+    }
+}
