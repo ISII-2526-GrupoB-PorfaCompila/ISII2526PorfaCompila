67e973d151995659084814a898e4276f511a02e1
Elias100cas <149321371+Elias100cas@users.noreply.github.com>
2025-10-21 11:24:03 +0200

    #87 Creado el método GET y su DTO de la seleccion de coches para el caso de uso Comprar Coches (#97)
    Aprobado

---
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -0,0 +1,31 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<CarsController> _logger;
+        public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForPurchaseDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCars_ForPurchase(string? color, string? model)
+        {
+            var cars = await _context.Cars.Include(Car => Car.Model)
+                .Where(c =>(c.Color.Contains(color) || (color == null)) && ((c.Model.Name.Contains(model)) || (model == null)))
+                .Select(c=>new CarForPurchaseDTO(c.Id, c.Model.Name, c.Color, c.Manufacturer, c.PurchasingPrice))
+                .ToListAsync();
+            return Ok(cars);
+        }
+    }
+}
+
