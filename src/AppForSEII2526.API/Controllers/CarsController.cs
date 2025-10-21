4ef5a0a038ef7f864d47271a53aa5ff632ccc038
Ángel Barceló Parada <angel.barcelo@alu.uclm.es>
2025-10-21 18:46:27 +0200

    84 creacioncontroladorpruebaangel (#113)
    * Controller a medias GET selección (prueba)
    
    * DTO creado para GET de la selección
    
    * Get para select del CU2-AlquilerCoches terminado
    
    * Añadido el archivo de los datos de prueba #84

--- src/AppForSEII2526.API/Controllers/CarsController.cs
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -1,4 +1,5 @@
 ﻿using AppForSEII2526.API.DTOs;
+using AppForSEII2526.API.DTOs.RentalDTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +9,18 @@ namespace AppForSEII2526.API.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
+        //used to enable your controller to access to the database
         private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
         private readonly ILogger<CarsController> _logger;
+
         public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
         {
             _context = context;
             _logger = logger;
         }
+
+        //Get para select del CU1 - Comprar coches
         [HttpGet]
         [Route("[action]")]
         [ProducesResponseType(typeof(IList<CarForPurchaseDTO>), (int)HttpStatusCode.OK)]
@@ -26,6 +32,20 @@ namespace AppForSEII2526.API.Controllers
                 .ToListAsync();
             return Ok(cars);
         }
+
+        //Get para select del CU2 - Alquilar coches
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForRentalDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCars_ForRental(double? rentingPrice, string? model)
+        {
+            var cars = await _context.Cars.Include(Car => Car.Model)
+                .Where(c => (c.RentingPrice <= rentingPrice || (rentingPrice == null)) && ((c.Model.Name.Contains(model)) || (model == null)))
+                .Select(c => new CarForRentalDTO(c.Id, c.Model.Name, c.Color, c.Manufacturer, c.FuelType, c.RentingPrice))
+                .ToListAsync();
+            return Ok(cars);
+        }
+
     }
 }
 
---
+++ src/AppForSEII2526.API/DTOs/RentalDTO/CarForRentalDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace AppForSEII2526.API.DTOs.RentalDTO
+{
+    public class CarForRentalDTO
+    {
+
+        /*2. El sistema muestra el conjunto de coches disponibles para alquilar desde hoy hasta la 
+        próxima semana, indicando el nombre del modelo, el tipo de gasoil, el fabricante, el 
+        precio del alquiler y el color. */
+
+        public CarForRentalDTO(int id, string model, string color, string manufacturer, string fuelType, double rentingPrice)
+        {
+            Id = id;
+            Model = model;
+            Color = color;
+            Manufacturer = manufacturer;
+            FuelType = fuelType;
+            RentingPrice = rentingPrice;
+        }
+
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Manufacturer { get; set; }
+        public string FuelType { get; set; }
+        public double RentingPrice { get; set; }
+    }
+}
