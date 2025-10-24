f6d8f1e45a5d5038ffbb98c45ebbdf8104d4d3a2
FRANCISCO SÁNCHEZ PONCE <Fco.Sanchez21@alu.uclm.es>
2025-10-24 12:19:30 +0200

    Hecho el DTO ReviewForCreateDTO y creados los demás DTO. #112

---
+++ src/AppForSEII2526.API/DTOs/ReviewDTO/ReviewDetailDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace AppForSEII2526.API.DTOs.ReviewDTO
+{
+    public class ReviewDetailDTO
+    {
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/ReviewDTO/ReviewForCreateDTO.cs
@@ -0,0 +1,37 @@
+﻿namespace AppForSEII2526.API.DTOs.ReviewDTO
+{
+    public class ReviewForCreateDTO
+    {
+        public ReviewForCreateDTO(string userName, string country, DriverTypes driverType, string? description, int rating, DateTime created, IList<ReviewItemDTO> reviewItems)
+        {
+            UserName = userName;
+            Country = country;
+            DriverType = driverType;
+            Description = description;
+            Rating = rating;
+            Created = created;
+            ReviewItems = reviewItems;
+        }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Country {  get; set; }
+
+        [Required]
+        public DriverTypes DriverType { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+        [Required]
+        [Display(Name = "Created Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Created { get; set; }
+
+        public IList<ReviewItemDTO> ReviewItems { get; set; }
+    }
+}
