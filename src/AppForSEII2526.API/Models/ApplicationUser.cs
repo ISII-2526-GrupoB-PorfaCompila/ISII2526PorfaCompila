8e16b85f177959f5c0e7a5335b3d3e45eb9bfd7d
Ángel Barceló Parada <angel.barcelo@alu.uclm.es>
2025-10-30 20:28:10 +0100

    107 crear método get del detalle para el caso de uso alquilar coches (#121)
    * Creados (no implementados) los DTOs restantes. #107
    
    * DTOs para el detail provisionales. #107
    
    * Hecho el GetDetalle provisional (todavía no lo he probado en el swagger)
    
    * Borradas migraciones para el Pull Request

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -54,6 +54,7 @@ public class ApplicationUser : IdentityUser {
     [Display(Name = "Surname")]
     public string? Surname { get; set; }
     public string? ClientPhoneNumber { get; set; }
+    public string? Address { get; set; }
 
     [Required]
     public IList<Purchase> Purchases { get; set; }
