cc2fb59f0e2c668685f7fe06eb0e8f1986cecd65
CARLOS JAVIER CAMACHO MARCHANTE <CarlosJavier.Camacho@alu.uclm.es>
2025-10-30 19:06:05 +0100

    Metodo getDetails CU3- ContratarMantenimientos #99

---
+++ src/AppForSEII2526.API/DTOs/ReservaDTO/ReservaItemDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace AppForSEII2526.API.DTOs.ReservaDTO
+{
+    public class ReservaItemDTO
+    {
+        public int ReservaId { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfDays { get; set; }
+        public string? Comentarios { get; set; }
+        public string Name { get; set; }
+
+        public ReservaItemDTO(int reservaId, string name, decimal price, int numberofdays, string? comentarios= "")
+        {
+            ReservaId = reservaId;
+            Name = name;
+            Price = price;
+            NumberOfDays = numberofdays;
+            Comentarios = comentarios;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReservaItemDTO dTO &&
+                   ReservaId == dTO.ReservaId &&
+                   Price == dTO.Price &&
+                   NumberOfDays == dTO.NumberOfDays &&
+                   Comentarios == dTO.Comentarios &&
+                   Name == dTO.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ReservaId, Price, NumberOfDays, Comentarios, Name);
+        }
+    }
+}
