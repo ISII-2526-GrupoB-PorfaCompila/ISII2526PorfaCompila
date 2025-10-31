7e4f66b2512f743c0d4e24d445bc69dfffe25bc1
FRANCISCO SÁNCHEZ PONCE <Fco.Sanchez21@alu.uclm.es>
2025-10-31 03:11:56 +0100

    Commit del Post de las  reviews a medio #117

--- src/AppForSEII2526.API/Controllers/ReviewsController.cs
+++ src/AppForSEII2526.API/Controllers/ReviewsController.cs
@@ -19,7 +19,7 @@ namespace AppForSEII2526.API.Controllers
 
         [HttpGet]
         [Route("[action]")]
-        [ProducesResponseType(typeof(ReviewDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(DTOs.ReviewDTO.ReviewDetailDTO), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<ActionResult> GetReview(int id)
         {
@@ -51,6 +51,90 @@ namespace AppForSEII2526.API.Controllers
 
 
             return Ok(review);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReviewDetailDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        public async Task<ActionResult> CreateReview(ReviewForCreateDTO reviewForCreate)
+        {
+            //any validation defined in ReviewForCreate is checked before running the method so they don't have to be checked again
+            if (reviewForCreate.ReviewItems.Count == 0)
+                ModelState.AddModelError("ReviewItems", "Error! You must include at least one car to be reviewed");
+
+            // if (!_context.ApplicationUsers.Any(au=>au.UserName==reviewForCreate.CustomerUserName))
+            var user = _context.ApplicationUsers.FirstOrDefault(au => au.UserName == reviewForCreate.UserName);
+            if (user == null)
+                ModelState.AddModelError("ReviewApplicationUser", "Error! UserName is not registered");
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+
+            var carIds = reviewForCreate.ReviewItems.Select(ri => ri.CarId).ToList();
+
+            var cars = _context.Cars.Include(c => c.ReviewItems)
+                .ThenInclude(ri => ri.Review)
+                .Where(c => carIds.Contains(c.Id))
+
+                //we use an anonymous type https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/anonymous-types
+                .Select(c => new {
+                    c.Id,
+                    c.Model.Name,
+                    c.FuelType,
+                    c.Manufacturer,
+                    c.Color,
+                })
+                .ToList();
+
+
+            Review review = new Review(reviewForCreate.UserName,reviewForCreate.Country,
+                reviewForCreate.Country, new List<RentalItem>());
+
+            foreach (var item in reviewForCreate.ReviewItems)
+            {
+                var car = cars.FirstOrDefault(c => c.Id == item.CarId);
+                //we must check that there is enough quantity to be rented in the database
+                if (car == null)
+                {
+                    ModelState.AddModelError("ReviewItems", $"Error! Car with id '{item.CarId}' is not available for being reviewed");
+                }
+                else
+                {
+                    // review does not exist in the database yet and does not have a valid Id, so we must relate reviewitem to the object review
+                    review.ReviewItems.Add(new ReviewItem(car.Id, review.Id, item.Description, item.Rating, car, review));
+                }
+            }
+
+            //if there is any problem because of the available quantity of movies or because the movie does not exist
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(review);
+
+            try
+            {
+                //we store in the database both rental and its rentalitems
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("Review", $"Error! There was an error while saving your review, plese, try again later");
+                return Conflict("Error" + ex.Message);
+
+            }
+
+            //it returns rentalDetail
+            var reviewDetail = new ReviewDetailDTO(review.Id, review.Created,
+                review.ApplicationUser.UserName, review.Country, review.DriverType,
+                reviewForCreate.ReviewItems);
+
+            return CreatedAtAction("GetReview", new { id = review.Id }, reviewDetail);
         }
     }
 }
