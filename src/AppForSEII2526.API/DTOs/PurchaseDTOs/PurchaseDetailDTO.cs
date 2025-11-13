19895821e0806e8fc9ee8e6a4850a844e4910033
Elias100cas <149321371+Elias100cas@users.noreply.github.com>
2025-11-13 17:51:16 +0100

    #87 Creado el método GET y su DTO del detalle compra coches para el caso de uso Comprar Coches (#130)

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseDetailDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class PurchaseDetailDTO
+    {
+
+        public PurchaseDetailDTO(int id, DateTime purchasingDate,string name, string surname, 
+                                string deliveryCarDealer, decimal purchasePrice, IList<PurchaseItemDTO> purchaseItems)
+        {
+            Id = id;
+            PurchasingDate = purchasingDate;
+            Name = name;
+            Surname = surname;
+            DeliveryCarDealer = deliveryCarDealer;
+            PurchasePrice = purchasePrice;
+            PurchaseItems = purchaseItems;
+        }
+        public int Id { get; set; }
+        public DateTime PurchasingDate { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string DeliveryCarDealer { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public IList<PurchaseItemDTO> PurchaseItems { get; set; }
+    }
+}
