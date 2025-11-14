893969790e4c5fe3eaf1a67593ab81b8482737f6
Elias100cas <149321371+Elias100cas@users.noreply.github.com>
2025-11-14 08:06:43 +0100

    #116 Creado metodo POST y DTOs para Comprar Coches
    Creado metodo POST con los DTOs y modificaciones pertinentes para el CU Comprar Coches

--- src/AppForSEII2526.API/Controllers/PurchaseController.cs
+++ src/AppForSEII2526.API/Controllers/PurchaseController.cs
@@ -1,4 +1,5 @@
 ﻿using AppForSEII2526.API.DTOs.PurchaseDTOs;
+using AppForSEII2526.API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,7 +21,7 @@ namespace AppForSEII2526.API.Controllers
         [Route("[action]")]
         [ProducesResponseType(typeof(PurchaseItemDTO), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
-        public async Task<IActionResult> GetBuy(int id)
+        public async Task<IActionResult> GetPurchase(int id)
         {
             if (_context.Purchases == null)
             {
@@ -42,10 +43,93 @@ namespace AppForSEII2526.API.Controllers
                     p.PurchaseItems.Select(pi => new PurchaseItemDTO(
                         pi.Car.Id,
                         pi.Car.Model.Name,
+                        pi.Car.Color,
+                        pi.Car.QuantityForPurchasing,
                         pi.Car.PurchasingPrice)).ToList()
                 )).FirstOrDefaultAsync();
 
             return Ok(purchase);
+        }
+
+
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(PurchaseDetailDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        public async Task<IActionResult> CreatePurchase([FromBody] PurchaseForCreateDTO purchaseForCreate)
+        {
+            if (purchaseForCreate.PurchaseItems.Count <= 0)
+            {
+                ModelState.AddModelError("PurchaseItems", "Error! You must include at least one car to be purchased");
+            }
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+            var user = await _context.Users.FirstOrDefaultAsync(au => au.Name == purchaseForCreate.Name && au.Surname == purchaseForCreate.Surname);
+            if (user == null)
+                ModelState.AddModelError("PurchaseApplicationUser", "Error! UserName is not registered");
+
+            var carsIds = purchaseForCreate.PurchaseItems.Select(pi => pi.CarId).ToList();
+
+            var cars = await _context.Cars.Include(c => c.PurchaseItems).ThenInclude(pi => pi.Purchase)
+                .Where(c => carsIds.Contains(c.Id))
+                .Select(c => new
+                {
+                    c.Id,
+                    c.Model.Name,
+                    c.Color,
+                    c.Description,
+                    c.PurchasingPrice,
+                }).ToListAsync();
+
+            Purchase purchase = new Purchase( purchaseForCreate.DeliveryCarDealer, DateTime.Now, user,
+                        new List<PurchaseItem>(), purchaseForCreate.PaymentMethod);
+
+            purchase.PurchasingPrice = 0;
+            foreach (var item in purchaseForCreate.PurchaseItems)
+            {
+                var car = cars.FirstOrDefault(c => c.Id == item.CarId);
+                if (car == null)
+                {
+                    ModelState.AddModelError("CarId", $"Error! The car with Id {item.CarId} does not exist.");
+                }
+                purchase.PurchaseItems.Add(new PurchaseItem(purchase, car.Id, item.QuantityForPurchase));
+                purchase.PurchasingPrice += car.PurchasingPrice * item.QuantityForPurchase;
+            }
+
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(purchase);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("Purchase", $"Error! There was an error while saving your purchase, plese, try again later");
+                return Conflict("Error" + ex.Message);
+
+            }
+
+            var purchaseDetail = new PurchaseDetailDTO
+            (
+                purchase.Id,
+                purchase.PurchasingDate,
+                purchaseForCreate.Name,
+                purchaseForCreate.Surname,
+                purchaseForCreate.DeliveryCarDealer,
+                purchase.PurchasingPrice,
+                purchaseForCreate.PurchaseItems
+            );
+            return CreatedAtAction("GetPurchase", new { id = purchase.Id }, purchaseDetail);
+
         }
     }
 }
---
+++ src/AppForSEII2526.API/Data/SeedData.cs
@@ -0,0 +1,6 @@
+﻿namespace AppForSEII2526.API.Data
+{
+    public class SeedData
+    {
+    }
+}
