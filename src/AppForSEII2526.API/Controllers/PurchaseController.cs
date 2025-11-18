dd1337f426d0020d7b8418a38f53431175edf7ed
Elias100cas <149321371+Elias100cas@users.noreply.github.com>
2025-11-18 12:50:01 +0100

    139 crear pruebas unitarias get detalle comprar coches (#151)
    * Inicio de las pruebas del get detalle
    
    * #139 Creadas las pruebas con las modificaciones pertinentes del GetDetalle

--- src/AppForSEII2526.API/Controllers/PurchaseController.cs
+++ src/AppForSEII2526.API/Controllers/PurchaseController.cs
@@ -10,8 +10,8 @@ namespace AppForSEII2526.API.Controllers
     public class PurchaseController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly ILogger<CarsController> _logger;
-        public PurchaseController(ApplicationDbContext context, ILogger<CarsController> logger)
+        private readonly ILogger<PurchaseController> _logger;
+        public PurchaseController(ApplicationDbContext context, ILogger<PurchaseController> logger)
         {
             _context = context;
             _logger = logger;
@@ -47,6 +47,11 @@ namespace AppForSEII2526.API.Controllers
                         pi.Car.QuantityForPurchasing,
                         pi.Car.PurchasingPrice)).ToList()
                 )).FirstOrDefaultAsync();
+
+            if (purchase == null)
+            {
+                return NotFound();
+            }
 
             return Ok(purchase);
         }
