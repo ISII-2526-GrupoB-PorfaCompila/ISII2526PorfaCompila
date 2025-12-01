aee859495dfdf092d6f5463675598a7c3867301d
ELÍAS LUCAS MONSALVE <Elias.Lucas@alu.uclm.es>
2025-12-01 10:04:59 +0100

    Commit StateContainer a medio hacer (Commit para bajarme lo nuevo de development) #176

---
+++ src/AppForSEII2526.Web/PurchaseStateContainer.cs
@@ -0,0 +1,42 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class PurchaseStateContainer
+    {
+        public PurchaseForCreateDTO Purchase { get; private set; } = new PurchaseForCreateDTO() { 
+            PurchaseItems = new List<PurchaseItemDTO>()
+            };
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Convert.ToDecimal(Purchase.PurchaseItems.Sum(pi => pi.PriceForPurchase * pi.QuantityForPurchase));
+            }
+        }
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+        public void AddPurchaseItem(CarForPurchaseDTO car)
+        {
+            if (!Purchase.PurchaseItems.Any(pi => pi.CarId == car.Id)) 
+            {
+                Purchase.PurchaseItems.Add(new PurchaseItemDTO()
+                {
+                    CarId = car.Id,
+                    Model = car.Model,
+                    Color = car.Color,
+                    PriceForPurchase = car.PurchasingPrice,
+                    QuantityForPurchase = 1,
+                });
+            }
+        }
+
+        public void RemovePurchaseItem(PurchaseItemDTO item)
+        {
+            Purchase.PurchaseItems.Remove(item);
+        }
+    }
+}
