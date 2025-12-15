ebdcb9ff1ff93c754036cb27612debb1aeaf9453
ÁNGEL BARCELÓ PARADA <Angel.Barcelo@alu.uclm.es>
2025-12-15 20:19:51 +0100

    Me he quedado por el este commit (we develop the test case for UC2_6) #210

--- test/AppForSEII2526.UIT/UC_Rental/UC_RentalCars_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalCars_UIT.cs
@@ -1,6 +1,7 @@
 ﻿using AppForMovies.UIT.Shared;
 using AppForSEII2526.UIT.Shared;
 using System;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,12 @@ namespace AppForSEII2526.UIT.UC_Rental
         private const string carColor1 = "Black";
         private const string carFuelType1 = "Petrol";
         private const string carManufacturer1 = "Audi";
+
+        private const string carRentingPrice2 = "45";
+        private const string carModel2 = "Corolla";
+        private const string carColor2 = "Silver";
+        private const string carFuelType2 = "Petrol";
+        private const string carManufacturer2 = "Toyota";
 
         public UC_RentalCars_UIT(ITestOutputHelper output) : base(output)
         {
@@ -38,21 +45,40 @@ namespace AppForSEII2526.UIT.UC_Rental
             _driver.FindElement(By.Id("CreateRental")).Click();
         }
 
-        [Fact]
+        //[Fact]
+        //[Trait("LevelTesting", "Funcional Testing")]
+        //public void UC2_AF1_UC2_4_5_6_filtering()
+        //{
+        //    //Arrange
+        //    InitialStepsForRentalCars();
+        //    var expectedCars = new List<string[]> { new string[] { carRentingPrice1, carModel1, carColor1, carFuelType1, carManufacturer1 }, };
+
+        //    //Act
+        //    selectCarsForRental_PO.SearchMovies("", "A3", "", "");
+
+        //    //Assert
+
+        //    Assert.True(selectCarsForRental_PO.CheckListOfCars(expectedCars));
+
+        //}
+
+        [Theory]
+        [InlineData(carRentingPrice1, carModel1, carColor1, carFuelType1, carManufacturer1, "A3", "")]
+        [InlineData(carRentingPrice2, carModel2, carColor2, carFuelType2, carManufacturer2, "", "45")]
         [Trait("LevelTesting", "Funcional Testing")]
-        public void UC2_AF1_UC2_4_5_6_filtering()
+        public void UC2_AF1_UC2_4_5_6_filtering(string carRentingPrice, string carModel, string carColor, string carFuelType, string carManufacturer, string filterModel, string filterRentingPrice)
         {
             //Arrange
             InitialStepsForRentalCars();
-            var expectedCars = new List<string[]> { new string[] { carRentingPrice1, carModel1, carColor1, carFuelType1, carManufacturer1 }, };
+            var expectedCars = new List<string[]> { new string[] { carRentingPrice, carModel, carColor, carFuelType, carManufacturer }, };
 
             //Act
-            selectCarsForRental_PO.SearchMovies("", "A3", "", "");
+            selectCarsForRental_PO.SearchMovies(filterRentingPrice, filterModel, "", "");
 
             //Assert
-
             Assert.True(selectCarsForRental_PO.CheckListOfCars(expectedCars));
 
         }
+
     }
 }
