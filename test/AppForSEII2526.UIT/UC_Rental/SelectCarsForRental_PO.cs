c438cd8170e6e4c2bd310f47048cd2d33761cdd6
ÁNGEL BARCELÓ PARADA <Angel.Barcelo@alu.uclm.es>
2025-12-15 19:42:05 +0100

    Antes del seed #210

--- test/AppForSEII2526.UIT/UC_Rental/SelectCarsForRental_PO.cs
+++ test/AppForSEII2526.UIT/UC_Rental/SelectCarsForRental_PO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
 
 namespace AppForSEII2526.UIT.UC_Rental
 {
@@ -10,18 +11,36 @@ namespace AppForSEII2526.UIT.UC_Rental
     {
         By inputPrice = By.Id("inputPrice");
         By inputGenre = By.Id("selectGenre");
+        By inputFrom = By.Id("fromDate");
+        By inputTo = By.Id("toDate");
+        By tableOfCarsBy = By.Id("TableOfCars");
+
         By buttonSearchCars = By.Id("searchCars");
         public SelectCarsForRental_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
         }
-        public void SearchMovies(string title)
+        public void SearchMovies(string price, string model, string from, string to)
         {
             //wait for the webelement to be clickable
             WaitForBeingClickable(inputPrice);
-            _driver.FindElement(inputPrice).SendKeys(title);
+            _driver.FindElement(inputPrice).SendKeys(price);
+            if (model == "") model = "All";
+            SelectElement selectElement = new SelectElement(_driver.FindElement(inputGenre));
+            selectElement.SelectByText(model);
+
             _driver.FindElement(buttonSearchCars).Click();
 
+            if (from != "")
+                _driver.FindElement(inputFrom).SendKeys(from);
+            if (to != "")
+                _driver.FindElement(inputTo).SendKeys(to);
 
         }
+
+        public bool CheckListOfCars(List<string[]> expectedCars)
+        {
+            return CheckBodyTable(expectedCars, tableOfCarsBy);
+        }
+
     }
 }
--- test/AppForSEII2526.UIT/UC_Rental/UC_RentalCars_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalCars_UIT.cs
@@ -10,8 +10,49 @@ namespace AppForSEII2526.UIT.UC_Rental
 {
     public class UC_RentalCars_UIT : UC_UIT
     {
+        private SelectCarsForRental_PO selectCarsForRental_PO;
+        private const int carId1 = 1;
+        private const string carRentingPrice1 = "60";
+        private const string carModel1 = "A3";
+        private const string carColor1 = "Black";
+        private const string carFuelType1 = "Petrol";
+        private const string carManufacturer1 = "Audi";
+
         public UC_RentalCars_UIT(ITestOutputHelper output) : base(output)
         {
+            selectCarsForRental_PO = new SelectCarsForRental_PO(_driver, _output);
+        }
+
+        //NO aplica porque no estamos usando login
+        //private void Precondition_perform_login()
+        //{
+        //    Perform_login("elena@uclm.es", "Password1234%");
+        //}
+
+        private void InitialStepsForRentalCars()
+        {
+            //Precondition_perform_login();
+            //we wait for the option of the menu to be visible
+            //selectMoviesForRental_PO.WaitForBeingVisible(By.Id("CreateRental"));
+            //we click on the menu
+            _driver.FindElement(By.Id("CreateRental")).Click();
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_AF1_UC2_4_5_6_filtering()
+        {
+            //Arrange
+            InitialStepsForRentalCars();
+            var expectedCars = new List<string[]> { new string[] { carRentingPrice1, carModel1, carColor1, carFuelType1, carManufacturer1 }, };
+
+            //Act
+            selectCarsForRental_PO.SearchMovies("", "A3", "", "");
+
+            //Assert
+
+            Assert.True(selectCarsForRental_PO.CheckListOfCars(expectedCars));
+
         }
     }
 }
