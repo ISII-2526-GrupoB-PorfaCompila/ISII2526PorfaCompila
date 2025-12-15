e3dc9f06ee33e991d96c82446255e0328db77c04
ELÍAS LUCAS MONSALVE <Elias.Lucas@alu.uclm.es>
2025-12-15 09:18:04 +0100

    Completadas modificaciones en detail y completado el POST #193

--- src/AppForSEII2526.API/Controllers/PurchaseController.cs
+++ src/AppForSEII2526.API/Controllers/PurchaseController.cs
@@ -19,7 +19,7 @@ namespace AppForSEII2526.API.Controllers
 
         [HttpGet]
         [Route("[action]")]
-        [ProducesResponseType(typeof(PurchaseItemDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(PurchaseDetailDTO), (int)HttpStatusCode.OK)]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public async Task<IActionResult> GetPurchase(int id)
         {
@@ -44,7 +44,7 @@ namespace AppForSEII2526.API.Controllers
                         pi.Car.Id,
                         pi.Car.Model.Name,
                         pi.Car.Color,
-                        pi.Car.QuantityForPurchasing,
+                        pi.Quantity,
                         pi.Car.PurchasingPrice)).ToList()
                 )).FirstOrDefaultAsync();
 
