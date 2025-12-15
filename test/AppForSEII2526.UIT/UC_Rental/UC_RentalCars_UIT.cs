ab6b5e5e73d6521e31836500c6057311f627e45b
ÁNGEL BARCELÓ PARADA <Angel.Barcelo@alu.uclm.es>
2025-12-15 18:36:29 +0100

    Commit raro #210

---
+++ test/AppForSEII2526.UIT/UC_Rental/UC_RentalCars_UIT.cs
@@ -0,0 +1,17 @@
+﻿using AppForMovies.UIT.Shared;
+using AppForSEII2526.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Rental
+{
+    public class UC_RentalCars_UIT : UC_UIT
+    {
+        public UC_RentalCars_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+    }
+}
