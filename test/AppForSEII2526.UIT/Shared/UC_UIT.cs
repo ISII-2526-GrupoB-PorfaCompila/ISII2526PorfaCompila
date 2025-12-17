547efe3cf69350e3c31513f58acdb9020c3d1189
PacoSanchez26 <160671423+PacoSanchez26@users.noreply.github.com>
2025-12-17 20:05:06 +0100

    213 desarrollar uit para selectcarsforreview (#225)
    * Commit con los filtros #213
    
    * Hechas las pruebas de los filtros y el carrito no disponible #213
    
    * Hechas las pruebas funcionales del Select #213
    
    * Hechas las pruebas funcionales de modificar el carrito, error al meter los datos, y modificar el carrito sin perder datos #213
    
    * Realizadas pruebas funcionales UC4-Reseñar Coches #213

--- test/AppForSEII2526.UIT/Shared/UC_UIT.cs
+++ test/AppForSEII2526.UIT/Shared/UC_UIT.cs
@@ -20,8 +20,8 @@ namespace AppForMovies.UIT.Shared {
         public string _URI {
             get {
                 //set url of your web page 
-                return "https://localhost:44335/";
-
+                return "https://localhost:7081/";
+                //return "https://localhost:44335/";
             }
         }
 
@@ -55,7 +55,7 @@ namespace AppForMovies.UIT.Shared {
                 .GoToUrl(_URI);
         }
 
-        protected void Perform_login(string email, string password) {
+        /*protected void Perform_login(string email, string password) {
             _driver.Navigate()
                     .GoToUrl(_URI + "Account/Login");
             // _driver.FindElement(By.Id("Input_Email"))
@@ -68,7 +68,7 @@ namespace AppForMovies.UIT.Shared {
 
             _driver.FindElement(By.XPath("/html/body/div[1]/main/article/div/div[1]/section/form/div[4]/button"))
                 .Click();
-        }
+        }*/
 
 
         protected void SetUp_Chrome4UIT() {
---
+++ test/AppForSEII2526.UIT/UC_Review/SelectCarsForReview_PO.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.UC_Review
+{
+    public class SelectCarsForReview_PO : PageObject
+    {
+        By inputManufacturer = By.Id("manufacturer");
+        By inputFuelType = By.Id("fuelType");
+        By buttonSearchCars = By.Id("searchCars");
+        By tableOfCarsBy = By.Id("TableOfCars");
+        By errorShownBy = By.Id("ErrorsShown");
+        By buttonReviewCars = By.Id("reviewCarButton");
+        public SelectCarsForReview_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchCars(string manufacturer, string fuelType)
+        {
+            //wait for the webelement to be clickable
+            WaitForBeingClickable(inputManufacturer);
+            _driver.FindElement(inputManufacturer).SendKeys(manufacturer);
+            WaitForBeingClickable(inputFuelType);
+            _driver.FindElement(inputFuelType).SendKeys(fuelType);
+            _driver.FindElement(buttonSearchCars).Click();
+        }
+
+        public void ReviewCars()
+        {
+            WaitForBeingClickable(buttonReviewCars);
+            _driver.FindElement(buttonReviewCars).Click();
+        }
+
+        public bool CheckListOfCars(List<string[]> expectedCars)
+        {
+
+            return CheckBodyTable(expectedCars, tableOfCarsBy);
+        }
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual Message shown:{actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+        public void AddCarToReviewingCart(int carId)
+        {
+            WaitForBeingClickable(By.Id("carToReview_" + carId));
+            _driver.FindElement(By.Id("carToReview_" + carId)).Click();
+        }
+
+        public void RemoveCarFromReviewingCart(int carId)
+        {
+            WaitForBeingClickable(By.Id("removeCar_" + carId));
+            _driver.FindElement(By.Id("removeCar_" + carId)).Click();
+        }
+
+        public bool ReviewingNotAvailable()
+        {
+            //the button is not Displayed=hidden
+
+            return _driver.FindElement(buttonReviewCars).Displayed == false;
+        }
+
+        public int CountCarsInReviewCart()
+        {
+            return _driver.FindElements(By.CssSelector("button[id^='removeCar_']")).Count;
+        }
+
+        // Método para verificar si un coche específico está en el carrito
+        /*public bool IsCarInReviewCart(int carId)
+        {
+            return _driver.FindElements(By.Id("removeCar_" + carId)).Count > 0;
+        }*/
+    }
+}
